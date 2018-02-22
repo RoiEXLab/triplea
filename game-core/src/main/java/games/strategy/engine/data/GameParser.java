5c3d7445d083c00c98134c95a9940dc9e791cf72
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-02-22 21:44:15 +0100

    Use GameParseException

--- game-core/src/main/java/games/strategy/engine/data/GameParser.java
+++ game-core/src/main/java/games/strategy/engine/data/GameParser.java
@@ -1266,9 +1266,9 @@ public final class GameParser {
             .orElseThrow(() -> new GameParseException("Missing property definition for option: " + name))
             .setValue(itemValues);
       } catch (final GameParseException e) {
-        throw new GameParseException(e);
+        throw e;
       } catch (final Exception e) {
-        throw new IllegalStateException("Unexpected Exception while setting values for attachment" + attachment, e);
+        throw new GameParseException("Unexpected Exception while setting values for attachment" + attachment, e);
       }
 
       options.add(Tuple.of(name, itemValues));
